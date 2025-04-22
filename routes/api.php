fa9978306fe65ce8fc998e17034238d3a525c5c7
davyaugustnurismail <davyaugustnurismail@gmail.com>
2025-04-22 10:20:24 +0700

    cors cofig 4

--- routes/api.php
+++ routes/api.php
@@ -22,15 +22,12 @@ Route::prefix('history')->group(function () {
 
 Route::prefix('auth')->group(function (){
 Route::post('/register', [AuthController::class, 'register'])
-    ->middleware('guest')
     ->name('register');
 
 Route::post('/login', [AuthController::class, 'login'])
-    ->middleware('guest')
     ->name('login');
 
 Route::post('/logout', [AuthController::class, 'logout'])
-    ->middleware('auth:sanctum')
     ->name('logout');
 
 Route::get('/me', [UserController::class, 'profile'])
