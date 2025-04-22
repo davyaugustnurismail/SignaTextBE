5781511c2facf483f0bec357d79f3f864ee6c3b5
davyaugustnurismail <davyaugustnurismail@gmail.com>
2025-04-22 11:06:02 +0700

    config 5

--- routes/api.php
+++ routes/api.php
@@ -27,8 +27,9 @@ Route::post('/register', [AuthController::class, 'register'])
 Route::post('/login', [AuthController::class, 'login'])
     ->name('login');
 
-Route::post('/logout', [AuthController::class, 'logout'])
-    ->name('logout');
+    Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout')
+    ->middleware('auth:sanctum');
 
 Route::get('/me', [UserController::class, 'profile'])
     ->middleware('auth:sanctum');
