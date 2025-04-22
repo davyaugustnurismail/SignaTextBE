66be97a8508b609d0d38c3ac42917faa14038e20
davyaugustnurismail <110774442+davyaugustnurismail@users.noreply.github.com>
2025-04-22 22:22:25 +0700

    config 8

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except:[
             'api/*'
         ]);
+        
+        $middleware->api(prepend: [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
